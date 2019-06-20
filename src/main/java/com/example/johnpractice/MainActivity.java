86369eac66e3aeaa9ed1a2433ba86afb230cd255
John <s346039@gmail.com>
2019-06-20 15:09:31 +0800

    init

---
+++ src/main/java/com/example/johnpractice/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.johnpractice;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+//負責這個APP執行的activity
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //必須複寫onCreate method
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);  //設定了這個activity預設的ContentView(內容畫面)  為R.layout.activity_main
+        //其中R代表幫我們自動產生的類別 => 所有在res的項目，都被包含在這個類別 =>這個johnpractice的預設activity(MainActivity)的使用者介面設定為activity_main.xml
+
+//        mTextMessage = (TextView) findViewById(R.id.message);
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
